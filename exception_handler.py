1dad547a186123f4f0e98c4c8aec339e35066fe9
Anmsh27 <91868364+Anmsh27@users.noreply.github.com>
2021-11-07 08:20:43 +0530

    Ad ded comments to exception_handler.py

--- exception_handler.py
+++ exception_handler.py
@@ -1,22 +1,22 @@
 import pygame
 
-
+# Custom class for factorial exception
 class FactorialException(Exception):
     pass
 
-
+# Generic error msg
 def value_error(surface):
     errorfont = pygame.font.Font(None, 100)
     rendered_result = errorfont.render('Error', True, (0, 0, 0))
     surface.blit(rendered_result, (0, 136))
 
-
+# Zero division error msg
 def zero_division_error(surface):
     errorfont1 = pygame.font.Font(None, 48)
     rendered_result1 = errorfont1.render('Error: Dividing by zero', True, (0, 0, 0))
     surface.blit(rendered_result1, (0, 168))
 
-
+Factorial exception msg
 def factorial_error(surface):
     errorfont2 = pygame.font.Font(None, 48)
     rendered_result3 = errorfont2.render('Error:', True, (0, 0, 0))
@@ -24,7 +24,7 @@ def factorial_error(surface):
     surface.blit(rendered_result3, (0, 136))
     surface.blit(rendered_result2, (0, 168))
 
-
+Overflow error msg
 def overflow_error(surface):
     errorfont = pygame.font.Font(None, 100)
     rendered_result = errorfont.render('Math error', True, (0, 0, 0))
